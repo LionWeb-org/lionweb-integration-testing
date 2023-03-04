a3799ee3e68683d9783f8f222eb168f8feb0105a
Meinte Boersma <meinte.boersma@gmail.com>
2023-03-04 18:51:50 +0100

    make cloning asynchronous => potentially concurrent

--- deno/src/clone.ts
+++ deno/src/clone.ts
@@ -7,20 +7,32 @@ const repos = {
     MPS: "lioncore-mps",
     OpenAPI: "lioncore-openapi",
     TypeScript: "lioncore-typescript"
-}
+} as const
 
 const branch = "main"
 const reposDir = "repos"
 
 const token = Deno.env.get("LIONWEB_PAT")
 
-for await (const [name, id] of Object.entries(repos)) {
-    console.log(`Cloning "${branch}" branch of ${name}-repo "${id}"...`)
-    const localRepoDir = `${reposDir}/${id}`
-    await exec(`rm -rf ${localRepoDir}`)
-    const url = `https://${token === undefined ? "" : `${token}@`}github.com/LIonWeb-org/${id}`
-    await exec(`npx tiged --mode=git --force ${url} -b ${branch} ${localRepoDir}`)
-    console.log(`...done`)
-    console.log()
+
+const cloneRepo = (name: string, repoId: string) => {
+    const message = `"${branch}" branch of ${name}-repo "${repoId}"`
+    console.log(`Cloning ${message}...`)
+    const localRepoDir = `${reposDir}/${repoId}`
+    const url = `https://${token === undefined ? "" : `${token}@`}github.com/LIonWeb-org/${repoId}`
+    return exec(`rm -rf ${localRepoDir}`)
+        .then(() => exec(`npx tiged --mode=git --force ${url} -b ${branch} ${localRepoDir}`))
+        .then(() => {
+            console.log(`...done cloning ${message}`)
+        })
 }
+
+
+Promise.all(
+        Object.entries(repos)
+            .map(([name, id]) => cloneRepo(name, id))
+    )
+    .then(() => {
+        console.log(`\nDone cloning all LIonWeb repositories.`)
+    })
 
