cd0be20e3d3913e78a4837a7cdffff43ae5bc2d6
Meinte Boersma <meinte.boersma@gmail.com>
2023-05-18 10:17:53 +0200

    (update some dependencies)

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
 import {exec} from "https://deno.land/x/exec@0.0.5/mod.ts"
 
-import {assertEquals, fail} from "https://deno.land/std@0.168.0/testing/asserts.ts"
+import {assertEquals, fail} from "https://deno.land/std@0.187.0/testing/asserts.ts"
 
 import {YamlLoader} from "https://deno.land/x/yaml_loader@v0.1.0/mod.ts"
 
