e738cfc2f5f5eefbfcdacbcbdd16513cfe1f3ae4
Meinte Boersma <meinte.boersma@gmail.com>
2023-09-22 08:58:33 +0200

    use @lionweb NPM packages
      (instead of pointing directly into that repository)
      + remove last JSON validation stuff from TS source

--- src/m3-instances.test.ts
+++ src/m3-instances.test.ts
@@ -1,8 +1,9 @@
 import {assertEquals} from "./deps.ts"
-import {readFileAsJson} from "../../repos/lioncore-typescript/src-utils/json.ts"
-import {sortSerialization} from "../../repos/lioncore-typescript/src-utils/serialization-utils.ts"
-import {deserializeLanguage} from "../../repos/lioncore-typescript/src/m3/deserializer.ts"
-import {SerializationChunk} from "../../repos/lioncore-typescript/src/serialization.ts"
+
+import {sortedSerialization} from "lionweb-utilities"
+import {deserializeLanguage, SerializationChunk} from "lionweb-core"
+
+import {readFileAsJson} from "./utils/json.ts"
 import {fromRoot, pathOfSerialization} from "./config.ts"
 
 
@@ -16,7 +17,7 @@ Deno.test("M3 instances (Deno)", async (tctx) => {
     await tctx.step("check whether Java and TypeScript serializations match", async () => {
         const serializationJava = await readFileAsJson(fromRoot(pathOfSerialization("m3", "Java"))) as SerializationChunk
         const serializationTypeScript = await readFileAsJson(fromRoot(pathOfSerialization("m3", "TypeScript"))) as SerializationChunk
-        assertEquals(sortSerialization(serializationJava), sortSerialization(serializationTypeScript))
+        assertEquals(sortedSerialization(serializationJava), sortedSerialization(serializationTypeScript))
     })
 
 })
