a34c2db11035bfc7325aa567c5055af6c409afbd
Meinte Boersma <meinte.boersma@gmail.com>
2023-11-10 14:06:38 +0100

    work around npx not being available in a GitHub Action

--- test.sh
+++ test.sh
@@ -2,7 +2,14 @@
 
 deno task run-tests
 
+# install AJV:
+npm init -y
+npm install ajv
+npm install ajv-cli
+npm install ajv-formats
+
+# validate schema using AJV:
 TS_LIONCORE_M3_SCHEMA=../repos/lionweb-typescript/packages/artifacts/schemas/lioncore_m3.serialization.schema.json
 JAVA_LIONCORE_SERIALIZATION=../repos/lionweb-java/core/src/test/resources/serialization/lioncore.json
-npx ajv -c ajv-formats --spec=draft2020 --strict=true --allErrors=true --allowUnionTypes=true test -s $TS_LIONCORE_M3_SCHEMA -d $JAVA_LIONCORE_SERIALIZATION --valid
+./node_modules/.bin/ajv -c ajv-formats --spec=draft2020 --strict=true --allErrors=true --allowUnionTypes=true test -s $TS_LIONCORE_M3_SCHEMA -d $JAVA_LIONCORE_SERIALIZATION --valid
 
